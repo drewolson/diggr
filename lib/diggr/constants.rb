3a645f1795dabb408326f71707f8a115f560f074
Drew Olson <drew@drewolson.org>
2008-08-26 22:59:53 -0700

    added valid root method names to Diggr::Constants

--- lib/diggr/constants.rb
+++ lib/diggr/constants.rb
@@ -1,4 +1,19 @@
 module Diggr
   module Constants
+    VALID_ROOT_METHODS = [
+      :containers,
+      :container,
+      :errors,
+      :error,
+      :galleryphotos,
+      :galleryphoto,
+      :media,
+      :medium,
+      :stories,
+      :story,
+      :topic,
+      :users,
+      :user
+    ]
   end
 end
