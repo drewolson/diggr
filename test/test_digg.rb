a095dedef4a08e44dd639cfb3959f3687b5cb2c9
Drew Olson <drew@drewolson.org>
2008-08-26 23:00:25 -0700

    added tests for valid and invalid root methods being called on Diggr::Digg object object

--- test/test_digg.rb
+++ test/test_digg.rb
@@ -2,11 +2,29 @@ require 'rubygems'
 require 'test/unit'
 require 'need'
 need { '../lib/diggr/digg' }
+need { '../lib/diggr/request' }
+need { '../lib/diggr/constants' }
 
 class TestDigg < Test::Unit::TestCase
   def test_instantiate_class
     assert_nothing_raised do
       Diggr::Digg.new
+    end
+  end
+
+  def test_valid_root_method_returns_a_request_instance
+    digg = Diggr::Digg.new
+
+    Diggr::Constants::VALID_ROOT_METHODS.each do |method|
+      assert_instance_of Diggr::Request, digg.send(method)
+    end
+  end
+
+  def test_invalid_root_method_throws_error
+    digg = Diggr::Digg.new
+
+    assert_raises NoMethodError do
+      digg.foo
     end
   end
 end
