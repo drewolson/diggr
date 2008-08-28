93ca7c731826994d1fa8872e1d217cf897748401
Drew Olson <drew@drewolson.org>
2008-08-27 20:34:17 -0700

    more tests for request

--- test/test_request.rb
+++ test/test_request.rb
@@ -4,10 +4,6 @@ require 'need'
 need { '../lib/diggr/request' }
 need { '../lib/diggr/constants' }
 
-class Diggr::Request
-  attr_accessor :end_point
-end
-
 class TestRequest < Test::Unit::TestCase
   def test_request_instantiation
     assert_nothing_raised do
@@ -19,13 +15,46 @@ class TestRequest < Test::Unit::TestCase
     request = Diggr::Request.new
     request.media
 
-    assert_equal '/media', request.end_point
+    assert_equal '/media', request.send(:instance_variable_get,"@end_point")
   end
 
   def test_simple_end_point_with_args
     request = Diggr::Request.new
     request.error('123')
 
-    assert_equal '/error/123', request.end_point
+    assert_equal '/error/123', request.send(:instance_variable_get,"@end_point")
+  end
+
+  def test_complex_end_point
+    request = Diggr::Request.new
+    request.stories.popular
+
+    assert_equal '/stories/popular', request.send(:instance_variable_get,"@end_point")
+  end
+
+  def test_complex_end_point_with_args
+    request = Diggr::Request.new
+    request.stories.topic("technology").hot
+
+    assert_equal '/stories/topic/technology/hot', request.send(:instance_variable_get,"@end_point")
+  end
+
+  def test_request_with_options
+    request = Diggr::Request.new
+    request.topics.options(:count => 5, :offset => 3)
+
+    assert_equal '&count=5&offset=3', request.send(:instance_variable_get,"@options")
+  end
+
+  def test_cleanse_string_with_forward_slash
+    request = Diggr::Request.new
+
+    assert_equal '%2F', request.send(:cleanse,"/")
+  end
+
+  def test_cleanse_number
+    request = Diggr::Request.new
+
+    assert_equal 2, request.send(:cleanse,2)
   end
 end
