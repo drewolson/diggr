189ef837734d5d89643659fe14e93c61195b7c32
Drew Olson <drew@drewolson.org>
2008-08-27 20:05:10 -0700

    root methods called on digg objects now pass that method to the request object

--- lib/diggr/digg.rb
+++ lib/diggr/digg.rb
@@ -6,9 +6,10 @@ need { 'request' }
 module Diggr
   class Digg
     Diggr::Constants::VALID_ROOT_METHODS.each do |method|
-      #TODO forward method name and arguments to Request after instantiation
       define_method(method) do |*args|
-        Diggr::Request.new 
+        request = Diggr::Request.new 
+        request.send(method,*args)
+        request
       end
     end
   end
--- test/test_digg.rb
+++ test/test_digg.rb
@@ -5,6 +5,10 @@ need { '../lib/diggr/digg' }
 need { '../lib/diggr/request' }
 need { '../lib/diggr/constants' }
 
+class Diggr::Request
+  attr_accessor :end_point
+end
+
 class TestDigg < Test::Unit::TestCase
   def test_instantiate_class
     assert_nothing_raised do
@@ -26,5 +30,19 @@ class TestDigg < Test::Unit::TestCase
     assert_raises NoMethodError do
       digg.foo
     end
+  end
+
+  def test_valid_root_method_returns_request_with_corrent_endpoint
+    digg = Diggr::Digg.new
+    request = digg.media
+
+    assert_equal "/media", request.end_point
+  end
+
+  def test_valid_root_method_with_args_returns_request_with_corrent_endpoint
+    digg = Diggr::Digg.new
+    request = digg.media('123')
+
+    assert_equal "/media/123", request.end_point
   end
 end
