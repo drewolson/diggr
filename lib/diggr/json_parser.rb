fe771d4b2c286a5c64a09586fe3ec4a897f60bc4
Drew Olson <drew@drewolson.org>
2008-08-29 11:28:43 -0500

    added json parser skeleton

--- lib/diggr/json_parser.rb
+++ lib/diggr/json_parser.rb
@@ -1,6 +1,16 @@
+require 'rubygems'
+require 'json'
+
 module Diggr
   class JSONParser
     def parse(data)
+      build_objects_from_parsed_json(JSON.parse(data))
+    end
+
+    private
+
+    def build_objects_from_parsed_json(parsed_data)
+      parsed_data
     end
   end
 end
